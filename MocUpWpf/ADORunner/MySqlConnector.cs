7b77f64df1562a37226090a5ec0a31cd343dadbd
Michael Leipper <michael.leipper@decisioncapital.co.uk>
2019-10-08 16:41:57 +0100

    added class

---
+++ MocUpWpf/ADORunner/MySqlConnector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MocUpWpf.ADORunner
+{
+    public class MySqlConnector
+    {
+        private readonly string _connectionString;
+        public  MySqlConnector(string connstring)
+        {
+            _connectionString = connstring;
+
+        }
+
+        public dataFrame 
+    }
+}
